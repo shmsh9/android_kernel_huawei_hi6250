7eb8b52e4c319668838dbace557fb699e322a6d6
shmsh9 <virgile.carbonari@gmail.com>
2019-10-31 11:16:15 +0100

    first commit

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+LOCAL_DIR=`pwd`
+PREBUILT="$LOCAL_DIR/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-"
+
+mkdir -p out
+
+make ARCH=arm64 O=out CROSS_COMPILE=$PREBUILT pmos_hi6250_defconfig
+make ARCH=arm64 O=out CROSS_COMPILE=$PREBUILT -j1
